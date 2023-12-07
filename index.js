c800ec6f538fbd8c70494a6a0b60d379241f9a8c
Fanny Fadlilatunnisa <134306407+Fannyfdlsa@users.noreply.github.com>
2023-12-07 13:53:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ require('dotenv').config();
 const express = require('express');
 const universityRoutes = require('./src/routes/universityRoutes');
 const facultyRoutes = require('./src/routes/facultyRoutes');
+const majorRoutes = require('./src/routes/majorRoutes'); 
+const achievementRoutes = require('./src/routes/achievementRoutes');
+const alumniRoutes = require('./src/routes/alumniRoutes');
+const registrationRoutes = require('./src/routes/registrationRoutes');
 const app = express();
 
 app.use(express.json());
 
 // Routes for University
-app.use('/', universityRoutes);
+app.use('/university', universityRoutes);
 
 // Routes for Faculty
 app.use('/faculties', facultyRoutes);
+
+// Routes for majors
+app.use('/majors', majorRoutes); 
+
+// Routes for Achievement
+app.use('/achievements', achievementRoutes);
+
+// Routes for Alumni
+app.use('/alumni', alumniRoutes);
+
+// Routes for Registration Paths
+app.use('/registration', registrationRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
