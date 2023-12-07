38e86d3980e509ddaa66a8b36e19bae321f250c2
Fanny Fadlilatunnisa <134306407+Fannyfdlsa@users.noreply.github.com>
2023-12-07 13:55:34 +0700

    Update universityRoutes.js

--- src/routes/universityRoutes.js
+++ src/routes/universityRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const UniversityController = require('../controller/universityController');
 
 router.get('/', UniversityController.getAllUniversities);
+router.get('/:id', UniversityController.getUniversityById);
 
 module.exports = router;
