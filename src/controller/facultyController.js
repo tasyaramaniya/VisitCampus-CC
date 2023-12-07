0dfe3362bd6f7c8b0da7d06d47450cd0413318b6
Fanny Fadlilatunnisa <134306407+Fannyfdlsa@users.noreply.github.com>
2023-12-07 13:56:25 +0700

    Update facultyController.js

--- src/controller/facultyController.js
+++ src/controller/facultyController.js
@@ -1,31 +1,45 @@
 const FacultyModel = require('../models/facultyModel');
 
 const FacultyController = {
-  // Fungsi untuk mendapatkan semua fakultas
-  getAllFaculties: async (req, res) => {
-    try {
-      const faculties = await FacultyModel.getAllFaculties();
-      res.json(faculties);
-    } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  },
-
-  // Fungsi untuk mendapatkan fakultas berdasarkan id
-  getFacultyById: async (req, res) => {
-    try {
-      const { id } = req.params;
-      const faculty = await FacultyModel.getFacultyById(id);
-
-      if (faculty) {
-        res.json(faculty);
-      } else {
-        res.status(404).json({ message: 'Fakultas tidak ditemukan' });
+    getAllFaculties: async (req, res) => {
+      try {
+        const faculties = await FacultyModel.getAllFaculties();
+        res.json(faculties);
+      } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
       }
-    } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  },
-};
+    },
 
-module.exports = FacultyController;
+    getFacultiesByUniversityId: async (req, res) => {
+        try {
+          const { universityId } = req.params;
+          const faculties = await FacultyModel.getFacultiesByUniversityId(universityId);
+    
+          if (!faculties) {
+            return res.status(404).json({ message: 'Fakultas tidak ditemukan' });
+          }
+    
+          res.json(faculties);
+        } catch (error) {
+          res.status(500).json({ error: 'Internal server error' });
+        }
+      },
+      
+      getMajorsByFacultyId: async (req, res) => {
+        try {
+          const { facultyId } = req.params;
+          const majors = await MajorModel.getMajorsByFacultyId(facultyId);
+    
+          if (!majors) {
+            return res.status(404).json({ message: 'Jurusan tidak ditemukan' });
+          }
+    
+          res.json(majors);
+        } catch (error) {
+          res.status(500).json({ error: 'Internal server error' });
+        }
+      },
+    };
+    
+  
+  module.exports = FacultyController;
