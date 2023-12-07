abab0cf0f4185da26f978f65649f6db569d9c108
Fanny Fadlilatunnisa <134306407+Fannyfdlsa@users.noreply.github.com>
2023-12-07 13:01:38 +0700

    Update facultyModel.js

--- src/models/facultyModel.js
+++ src/models/facultyModel.js
@@ -1,7 +1,6 @@
 const db = require('../config/database');
 
 const FacultyModel = {
-  // Fungsi untuk mendapatkan semua fakultas
   getAllFaculties: () => {
     return new Promise((resolve, reject) => {
       db.query('SELECT * FROM faculty', (err, results) => {
@@ -13,14 +12,13 @@ const FacultyModel = {
     });
   },
 
-  // Fungsi untuk mendapatkan fakultas berdasarkan nama
-  getFacultyById: (id) => {
+  getFacultiesByUniversityId: (universityId) => {
     return new Promise((resolve, reject) => {
-      db.query('SELECT * FROM faculty_id WHERE id = ?', [id], (err, results) => {
+      db.query('SELECT * FROM faculty WHERE university_id = ?', [universityId], (err, results) => {
         if (err) {
           reject(err);
         }
-        resolve(results[0]);
+        resolve(results);
       });
     });
   },
